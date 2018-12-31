8e0110031167c2816e443eb4a7019b59577c266f
ErikBoesen <me@erikboesen.com>
2018-12-31 00:24:17 -0500

    Add and link JS

---
+++ scripts.js
@@ -0,0 +1,3 @@
+document.getElementById('submit').onclick = function() {
+    document.getElementById('results').textContent = (document.getElementById('ea').checked ? 'You are EA so you get human rights regardless of any other societally condemned behaviors in which you may have partaken, congrats' : 'EW RD GROSS GET AWAY');
+};
