f16183b4701e6a3a69f01b5167d965dd697066d1
ErikBoesen <me@erikboesen.com>
2018-12-31 00:33:52 -0500

    Admire Carlos

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+const results = document.getElementById('results');
+
 document.getElementById('submit').onclick = function() {
-    document.getElementById('results').textContent = (document.getElementById('ea').checked ? 'You are EA so you get human rights regardless of any other societally condemned behaviors in which you may have partaken, congrats' : 'EW RD GROSS GET AWAY');
+    if (document.getElementById('carlos')) results.textContent = 'leave'
+    else results.textContent = (document.getElementById('ea').checked ? 'You are EA so you get human rights regardless of any other societally condemned behaviors in which you may have partaken, congrats' : 'EW RD GROSS GET AWAY');
 };
